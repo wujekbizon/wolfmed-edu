def0ab091bbc27fe614d23f6c3f9877a93579e41
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2024-08-28 08:59:37 +0200

    Implement custom hook for generating test, and helper function for randomize questions

--- src/components/GenerateTests.tsx
+++ src/components/GenerateTests.tsx
@@ -1,5 +1,29 @@
+'use client'
+
+import { useGenerateTestStore } from '@/store/useGenerateTestStore'
 import { Test } from '@/types/dataTypes'
+import TestsLevelMenu from './TestsLevelMenu'
+import { useGeneratedTest } from '@/hooks/useGeneratedTest'
 
 export default function GenerateTests(props: { tests: Test[] }) {
-  return <div>GenerateTests</div>
+  const { numberTests, isTest } = useGenerateTestStore()
+  const randomTest = useGeneratedTest(props.tests, numberTests)
+
+  return (
+    <section className="flex  w-full flex-col items-center gap-8 px-4">
+      {!isTest && <TestsLevelMenu />}
+
+      <form action="" className="flex w-full flex-col items-center justify-center gap-4">
+        {numberTests && (
+          <div className="grid w-full grid-cols-1 gap-4 pb-24 lg:grid-cols-2 xl:w-5/6">
+            {randomTest.map((item) => (
+              <div className="flex flex-col" key={item.data.question}>
+                {item.data.question}
+              </div>
+            ))}
+          </div>
+        )}
+      </form>
+    </section>
+  )
 }
