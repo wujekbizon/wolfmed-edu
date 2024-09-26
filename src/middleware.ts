3346b6fdfccbcaecb9ad18c342efae731ff13e07
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2024-09-26 19:56:38 +0200

    Fix Clerk middleware by adding unauthorized and unauthenticated redirect url

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,11 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isProtectedRoute = createRouteMatcher(['/testy-opiekun(.*)', '/blog(.*)'])
 
 export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) auth().protect()
+  if (isProtectedRoute(request))
+    auth().protect({
+      unauthorizedUrl: `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`,
+      unauthenticatedUrl: `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`,
+    })
 })
 
 export const config = {
