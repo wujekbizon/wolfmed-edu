57c88a0f30270714f99e2a45820317fa7d1aa033
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2024-10-08 19:32:33 +0200

    Tweaks google analytics

--- src/app/_components/GoogleAnalytics.tsx
+++ src/app/_components/GoogleAnalytics.tsx
@@ -1,22 +1,22 @@
 import Script from 'next/script'
 
 export default function GoogleAnalytics() {
+  const GA_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID || 'dev'
+  const GTAG_JS_URI = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
+
   return (
-    <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
-      >
+    <head>
+      <Script strategy="lazyOnload" async src={GTAG_JS_URI}>
         <Script>
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
-            page_path: window.location.pathname,
+            gtag('config', '${GA_ID}', {
+            page_path: window.location.pathname });
          `}
         </Script>
       </Script>
-    </>
+    </head>
   )
 }
