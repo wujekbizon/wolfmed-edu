421e85d91ed53d84bc59fe148c6e87abaccfaa34
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2024-10-14 14:45:17 +0200

    Few changes to about page

--- src/app/_components/About.tsx
+++ src/app/_components/About.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function About() {
   return (
-    <section className="relative h-[calc(100vh_-_70px)] w-full flex flex-col items-center justify-end bg-[#ffb5b5] bg-[url('/students.webp')] rounded-br-[46px] bg-top rounded-bl-[46px] bg-cover p-4 xs:p-12">
+    <section className="relative min-h-[calc(100vh_-_70px)] w-full flex flex-col items-center justify-end bg-[#ffb5b5] bg-[url('/students.webp')] rounded-br-3xl sm:rounded-br-[50px] bg-top rounded-bl-3xl sm:rounded-bl-[50px] bg-cover p-3 xs:p-12">
       <div className="container w-full md:w-3/4 xl:w-1/2 bg-[#ffb5b5]/90 p-4 sm:p-6 flex flex-col gap-3 rounded-2xl shadow-md shadow-zinc-500 border border-red-200/60">
         <h1 className="text-2xl sm:text-4xl font-bold py-2 text-center">
           WOLFMED <span className="text-zinc-500 font-semibold">EDUKACJA</span>
@@ -16,21 +16,22 @@ export default function About() {
           Oferujemy <span className="font-bold text-lg underline">bezpłatny</span> dostęp do najnowszych testów i pytań
           medycznych, które pomogą Ci rozwinąć swoją wiedzę i umiejętności.
         </p>
-        <p>
-          Jesteśmy w trakcie rozwoju dlatego zachęcamy każdego do wsparcia naszego startupu:
-          <span className="text-xl font-bold text-red-500 animate-pulse">
-            {' '}
-            <Link target="_blank" href="https://buymeacoffee.com/grzegorzwolfinger">
-              Kupuje serduszko ❤️
-            </Link>
-          </span>
+        <p className="text-base">
+          Dołącz do nas w budowaniu społeczności zmotywowanych osób zaangażowanych w pozytywny wpływ na opiekę
+          zdrowotną.
         </p>
-        <div className="flex flex-col w-full items-end gap-2">
-          <p className="text-base">
-            Dołącz do nas w budowaniu społeczności zmotywowanych osób zaangażowanych w pozytywny wpływ na opiekę
-            zdrowotną.
-          </p>
-        </div>
+        <p className="text-base">
+          Dołącz do nas w budowaniu społeczności zmotywowanych osób zaangażowanych w pozytywny wpływ na opiekę
+          zdrowotną.
+        </p>
+        <p className="text-base">Jesteśmy w trakcie rozwoju dlatego zachęcamy każdego do wsparcia naszego startupu: </p>
+        <Link
+          className="self-center w-full xs:w-64 text-center px-4 my-4 sm:px-6 py-2 sm:py-3 bg-red-600 hover:bg-red-700 focus:ring-red-500 text-white font-bold text-sm sm:text-base rounded-full transition-all duration-300 transform hover:scale-105"
+          target="_blank"
+          href="https://buymeacoffee.com/grzegorzwolfinger"
+        >
+          Kup serduszko ❤️
+        </Link>
       </div>
     </section>
   )
