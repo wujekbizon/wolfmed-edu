95ec78afb6d39734081b011181ad06bde5be4df7
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2024-11-03 23:45:02 +0100

    Switch to neon-serverless instead of neon-http driver, which provides full transaction support.

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,10 +1,10 @@
-import { neon } from '@neondatabase/serverless'
-import { drizzle } from 'drizzle-orm/neon-http'
+import { Pool } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/neon-serverless'
 import * as schema from './schema'
 
 if (!process.env.NEON_DATABASE_URL) {
   throw new Error('NEON_DATABASE_URL is not defined')
 }
 
-const sql = neon(process.env.NEON_DATABASE_URL)
-export const db = drizzle(sql, { schema })
+const pool = new Pool({ connectionString: process.env.NEON_DATABASE_URL })
+export const db = drizzle(pool, { schema })
