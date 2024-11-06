dddc2cf82da4a1597f5d0147b952cd135de9198b
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2024-11-06 17:43:06 +0100

    Add SpeedInsights to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import Navbar from './_components/Navbar'
@@ -62,6 +63,7 @@ export default function RootLayout({
               <ToastProvider>{children}</ToastProvider>
             </Providers>
           </main>
+          <SpeedInsights />
         </body>
       </html>
     </ClerkProviderWrapper>
