16d633fd3fcc209637534b445949aa87595ab781
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2025-01-10 19:40:30 +0100

    Refactor authentication middleware to streamline error handling and improve session protection logic;

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
 
 const isProtectedRoute = createRouteMatcher(['/testy-opiekun(.*)', '/blog(.*)', '/forum(.*)'])
 
 export default clerkMiddleware(async (auth, request) => {
-  try {
-    if (isProtectedRoute(request)) {
-      await auth.protect({
-        unauthorizedUrl: `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`,
-        unauthenticatedUrl: `${process.env.NEXT_PUBLIC_APP_URL}/sign-in`,
-      })
-    }
-  } catch (error: unknown) {
-    console.error('Auth protection error:', error)
-
-    if (error instanceof Error) {
-      // Check if the error message contains the 'Token refresh failed' string or network-related issue
-      if (error.message.includes('Token refresh failed') || error.message.includes('Failed to fetch')) {
-        // Handle session expiry or token refresh failure
-        const signInUrl = new URL('/sign-in', request.url)
-        signInUrl.searchParams.set('redirect_url', request.url) // Redirect back after sign-in
-        return NextResponse.redirect(signInUrl)
-      }
-    }
+  if (isProtectedRoute(request)) {
+    await auth.protect({
+      unauthorizedUrl: `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`,
+      unauthenticatedUrl: `${process.env.NEXT_PUBLIC_APP_URL}/sign-in`,
+    })
   }
-  return NextResponse.next()
 })
 
 export const config = {
