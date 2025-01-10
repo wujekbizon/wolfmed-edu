e1c272eb36218cad79097bdbd04300bdd45c4c54
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2025-01-10 15:05:37 +0100

    Refactor Next.js configuration for improved caching and Sentry integration; enable experimental PPR in forum post page

--- next.config.ts
+++ next.config.ts
@@ -27,14 +27,7 @@ const nextConfig: NextConfig = {
   },
   webpack: (config) => {
     config.cache = {
-      type: 'filesystem',
-      buildDependencies: {
-        config: [__filename],
-      },
-      cacheOptions: {
-        // Adjust cache serialization strategy
-        buffer: true,
-      },
+      type: 'memory',
     }
     return config
   },
@@ -42,23 +35,10 @@ const nextConfig: NextConfig = {
 export default withSentryConfig(nextConfig, {
   org: 'wesa-vs',
   project: 'wolfmed-edukacja',
-
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
   hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  sourcemaps: { disable: true },
   disableLogger: true,
   automaticVercelMonitors: true,
 })
