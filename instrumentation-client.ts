ba2f404c5774e30a8228e1c1a4a303f1a400aab0
wujekbizon <wujekbizon@gmail.com>
2025-07-16 16:10:16 +0200

    Fix warning about sentry.client.config being deprecated

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -13,3 +13,4 @@ Sentry.init({
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 });
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
