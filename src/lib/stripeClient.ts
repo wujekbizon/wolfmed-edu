8d98e03b31fb0c4f61df3386aaf683433c474d6e
wujekbizon <wujekbizon@gmail.com>
2026-01-09 09:19:10 +0100

    chore: update package dependencies and Stripe API version
    - Bumped versions of several dependencies in package.json for improved stability and features

--- src/lib/stripeClient.ts
+++ src/lib/stripeClient.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-11-17.clover',
+  apiVersion: '2025-12-15.clover',
 })
 
 export default stripe
