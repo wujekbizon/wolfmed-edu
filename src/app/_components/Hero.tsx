f71d8e6a0ae90de3b0efe200c10d46e258e75af0
wujekbizon <wujekbizon@gmail.com>
2026-04-11 13:35:47 +0200

    docs: adjust padding in Hero component for improved layout

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -3,7 +3,7 @@ import ScrollButton from '@/components/ScrollButton'
 
 export default function Hero() {
   return (
-    <section className="relative w-full min-h-[calc(100dvh-70px)] flex items-center justify-center overflow-hidden py-8 sm:py-12">
+    <section className="relative w-full min-h-[calc(100dvh-70px)] flex items-center justify-center overflow-hidden pt-8 pb-24 sm:py-12">
       <HeroContent />
       <ScrollButton tag="explore" className="bottom-8" />
     </section>
