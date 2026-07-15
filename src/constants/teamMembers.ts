e014435fc95a2415f6c68feb1c3f286c85bc9f2d
wujekbizon <wujekbizon@gmail.com>
2026-07-15 13:18:59 +0200

    Update image URL for Kinga Wolfinger in teamMembers constant

--- src/constants/teamMembers.ts
+++ src/constants/teamMembers.ts
@@ -2,7 +2,7 @@ export const teamMembers = [
   {
     name: 'Kinga Wolfinger',
     role: 'Założyciel i Główny Instruktor',
-    image: 'https://utfs.io/a/zw3dk8dyy9/UVAwLrIxs2k5lJJEon5QZL8RnOt9ME3WgkVAFJaXBNK7HC5q',
+    image: 'https://utfs.io/a/zw3dk8dyy9/UVAwLrIxs2k5a0ASQYktQrdmqhSKIRj5fanksB630Te2FpiO',
     bio: 'Doświadczony opiekun medyczny oraz opiekun osób starszych, z wieloletnim doświadczeniem.',
     linkedin: 'https://www.linkedin.com/in/kinga-wolfinger-71b0aa2a2/',
   },
